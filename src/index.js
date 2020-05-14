3c1a2b21f0d78b0034db1fbf2cc1e0de57f437c2
architolk <msj@xs4all.nl>
2020-05-14 08:55:00 +0200

    Removed comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-//import Diagram from './diagram.js'
-//import diagram from './diagram.js'
 import {initGraph, togglefullscreen, mouseoverPropertyBox, mouseoutPropertyBox, clickInfoBox, clickPropertyBox, expand, hideNode} from './diagramglobal.js'
 
 var jsonApiSubject = "http://example.org/id/JaneDoe"; //URI of the subject that is the center of the graph
@@ -7,10 +5,6 @@ var jsonApiIDSubject = ""; //Not used
 var jsonApiCall = "http://localhost:8080/test.json?subject="; //Graph that contains the data (or a JSON-LD service that can be queried)
 var uriEndpoint = "http://localhost:8080?subject="; //Endpoint that can be used to navigate to (empty means that the original URI is used as endpoint)
 var fragments = {};
-
-//var D = new Diagram();
-//D.start();
-//diagram.init(jsonApiSubject, jsonApiIDSubject, jsonApiCall, uriEndpoint, fragments);
 
 initGraph(jsonApiSubject, jsonApiIDSubject, jsonApiCall, uriEndpoint, fragments);
 
